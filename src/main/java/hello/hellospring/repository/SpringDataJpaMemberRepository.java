e01d29c3356d1af91c0b0a49e70beca8a8b8b3e0
wonjinSong <wjsong0204@naver.com>
2022-07-25 11:33:07 +0900

    study spring 0725 스프링 데이터 JPA

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+//SpringDataJpaMemberRepository 가 JpaRepository를 받고 있으면 구현체를 자동으로 만들어서 Spring Bean에 자동으로 등록해준다.
+// 기본적인 CRUD를 제공해 준다.
 public interface SpringDataJpaMemberRepository extends JpaRepository <Member, Long>, MemberRepository {
 
+    // JPQL select m from Member m where m.name = ?
     @Override
     Optional<Member> findByName(String name);
 }
